8485c1fcd02747b04ceac612a2bc74925f2ef8d3
Bertrand Andres <bertrand.andres.dev@gmail.com>
2021-08-31 22:26:25 +0200

    Removed dice rolls (will be managed by dice class)

--- src/Entity/CharacterEntity.php
+++ src/Entity/CharacterEntity.php
@@ -2,7 +2,6 @@
 namespace M20Online\Entity;
 
 use InvalidArgumentException;
-use LogicException;
 
 final class CharacterEntity extends EntityAbstract
 {
@@ -14,10 +13,6 @@ final class CharacterEntity extends EntityAbstract
     const SKILL_SUBTERFUGE = 'subterfuge';
     const SKILL_KNOWLEDGE = 'knowledge';
     const SKILL_COMMUNICATION = 'communication';
-
-    private array $statsList = [self::STAT_STR, self::STAT_DEX, self::STAT_MIND];
-
-    private array $skillsList = [self::SKILL_PHYSICAL, self::SKILL_SUBTERFUGE, self::SKILL_KNOWLEDGE, self::SKILL_COMMUNICATION];
 
     private array $statsBonusList = [
         self::STAT_STR => 0,
@@ -44,113 +39,68 @@ final class CharacterEntity extends EntityAbstract
 
     public function set(string $pKey, $pValue) : void
     {
-        if (in_array($pKey, $this->statsList)) {
+        if (array_key_exists($pKey, $this->statsBonusList)) {
             // it's a stat, calc bonus:
-            $this->statsbonus[$pKey] = floor(($pValue - 10) / 2);
+            $this->statsBonusList[$pKey] = floor(($pValue - 10) / 2);
         }
 
         parent::set($pKey, $pValue);
     }
 
-    /**
-     * To roll a stat, roll 4D6, delete lowest value then sum others.
-     * 
-     * @return int stat value
-     */
-    private function rollStatValue () : int
+    public function setSkillBonus (string $pSkill, int $pValue)
     {
-        $resultsList = [];
-
-        // Generate values
-        for ($i = 0; $i < 4; $i++) {
-            $resultsList[] = rand(1, 6);
-        }
-
-        // Drop lowest value
-        sort($resultsList);
-        array_shift($resultsList);
-
-        // Sum remaining values and return it: 
-        return array_sum($resultsList);
-    }
-
-    /**
-     * @param string $pStat stat name
-     * @return int stat value
-     */
-    public function generateStat (string $pStat) : void
-    {
-        if (!in_array($pStat, $this->statsList)) {
-            throw new InvalidArgumentException('Stat '.$pStat.' not found');
-        }
-        
-        $statValue = $this->rollStatValue();
-        $this->set($pStat, $statValue);
-    }
-
-    /**
-     * You roll HP value by adding 1D6 to your strength value
-     */
-    public function rollHp () : void
-    {
-        $statStr = $this->get(self::STAT_STR);
-
-        if (0 === $this->get($statStr)) {
-            throw new LogicException('You must roll strength stat before roll hp');
-        }
-
-        $this->set('hp', $statStr + rand(1, 6));
-    }
-
-    /**
-     * How to roll a skill check:
-     * 1D20 + bonus skill + level + stat bonus + any other bonus value
-     */
-    public function rollSkillCheck (string $pSkill, int $pOtherBonus = 0) : int
-    {
-        if (!in_array($pSkill, $this->skillsList)) {
-            throw new InvalidArgumentException('Skill '.$pSkill.' not found');
-        }
-
-        return rand(1, 20) + $this->skillsBonusList[$pSkill] + $this->getStatBonusFromSkill($pSkill) + $pOtherBonus;
-    }
-
-    public function applySkillBonus (string $pSkill, int $pValue)
-    {
-        if (!in_array($pSkill, $this->skillsList)) {
+        if (!array_key_exists($pSkill, $this->skillsBonusList)) {
             throw new InvalidArgumentException('Skill '.$pSkill.' not found');
         }
 
         $this->skillsBonusList[$pSkill] += $pValue;
     }
 
-    public function applyStatBonus (string $pStat, int $pValue)
+    public function getSkillBonus (string $pSkill) : int
     {
-        if (!in_array($pStat, $this->statBonusList)) {
+        if (!array_key_exists($pSkill, $this->skillsBonusList)) {
+            throw new InvalidArgumentException('Skill '.$pSkill.' not found');
+        }
+
+        return $this->skillsBonusList[$pSkill];
+    }
+
+    public function setStatBonus (string $pStat, int $pValue)
+    {
+        if (!array_key_exists($pStat, $this->statsBonusList)) {
             throw new InvalidArgumentException('Stat '.$pStat.' not found');
         }
 
         $this->statBonusList[$pStat] = $pValue;
     }
 
-    private function getStatBonusFromSkill ($pSkill) : int
+    public function getStatBonus (string $pStat) : int
+    {
+        if (!array_key_exists($pStat, $this->statsBonusList)) {
+            throw new InvalidArgumentException('Stat '.$pStat.' not found');
+        }
+
+        return $this->statBonusList[$pStat];
+    }
+
+    public function getStatBonusFromSkill ($pSkill) : int
     {
         if (self::SKILL_COMMUNICATION === $pSkill) {
-            return self::statBonusList[self::STAT_MIND];
+            return $this->statBonusList[self::STAT_MIND];
         }
 
         if (self::SKILL_KNOWLEDGE === $pSkill) {
-            return self::statBonusList[self::STAT_MIND];
+            return $this->statBonusList[self::STAT_MIND];
         }
 
         if (self::SKILL_PHYSICAL === $pSkill) {
-            return self::statBonusList[self::STAT_STR];
+            return $this->statBonusList[self::STAT_STR];
         }
 
         if (self::SKILL_SUBTERFUGE === $pSkill) {
-            return self::statBonusList[self::STAT_DEX];
+            return $this->statBonusList[self::STAT_DEX];
         }
 
-        throw new LogicException('Skill '.$pSkill.' not found.');
+        throw new InvalidArgumentException('Skill '.$pSkill.' not found.');
     }
 }
